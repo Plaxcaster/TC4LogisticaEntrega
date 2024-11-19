ef2fc58c2fb1890f1ba6567c7a5dd1a1ba736169
Pedro Plá <pedro.bpla@gmail.com>
2024-11-19 20:38:23 -0300

    commit inicial do Spring Initializr

---
+++ src/test/java/horizonleap/logistica/entrega/LogisticaEntregaApplicationTests.java
@@ -0,0 +1,13 @@
+package horizonleap.logistica.entrega;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogisticaEntregaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
